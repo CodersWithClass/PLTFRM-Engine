6f28e1a3b0d225d54187886ff01ba970acaeac37
Jiawei Chen <jchen2120212@gmail.com>
2014-12-03 10:28:25 -0500

    Improved parsing function of LevelRead.py and added another type of level file for it to work with.

--- LevelRead.py
+++ LevelRead.py
@@ -4,26 +4,46 @@ object_list = []#List of processed items ready for import to the PLTFRM engine
 import re
 for lines in level:
     if lines[0] != '' and lines[0] != '#':
+        
+        lines = re.sub(r'\s', '', lines)
         lines = re.split(':|\n|', lines)
         
         
         if len(lines) > 1:
             lines[1] = re.split(';', lines[1])
-            
-                        
+        
+      #  lines[0] = re.sub(r'\s', '', lines[0])    
+                      
+             
         if lines[0] != '':
             active_objects.append(lines)
 
+        
 
-            
 for lines in active_objects:
+    if len(lines) > 1:
+        for num in range(0, len(lines[1])):
+                try:
+                    lines[1][num] = int(lines[1][num])
+                    
+                except ValueError:
+                    print("Not an int")
 
-    if len(lines[1]) > 0:
-        for num in range(0, len(lines[1])): #Removes leading spaces in data entries
-            if lines[1][num][0] == ' ':
-                lines[1][num] = lines[1][num][1:]
-    object_list.append(lines)
+                    
+    print(lines)
     
-for items in object_list:
-    print(items)
+    #===========================================================================
+    # if len(lines[1]) > 1:
+    #     for num in range(0, len(lines[1])): #Removes leading spaces in data entries
+    #         if lines[1][num][0] == ' ':
+    #             lines[1][num] = lines[1][num][1:]
+    #             
+    # for num in range(0, len(lines)):
+    #     if lines[num] == '':
+    #         del(lines[num])
+    # object_list.append(lines)
+    #  
+    #===========================================================================
+#for items in object_list:
+ #   print(items)
 #raise TypeError('ERROR: this object required a number in its properties.')
